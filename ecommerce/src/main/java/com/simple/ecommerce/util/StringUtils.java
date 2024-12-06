ae2d2e09e4bc45fa9ec8046dce1d1adff25da367
tjdwns6872 <tjdwns6872@naver.com>
2024-12-06 20:04:31 +0900

    문자열 관련 커스텀 유틸리티 추가

---
+++ ecommerce/src/main/java/com/simple/ecommerce/util/StringUtils.java
@@ -0,0 +1,8 @@
+package com.simple.ecommerce.util;
+
+public class StringUtils {
+ 
+    public static boolean isStringEmpty(String str) {
+        return str == null || str.isBlank();    
+    }
+}
