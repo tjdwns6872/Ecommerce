cc93f10e97ce4189854bb9edd180e61a7f192a5e
tjdwns6872 <tjdwns6872@naver.com>
2024-12-12 20:26:17 +0900

    로그아웃 추가

--- client/src/assets/js/user/myPageEvent.js
+++ client/src/assets/js/user/myPageEvent.js
@@ -3,8 +3,11 @@ import ApiFactory from '../../../services/ApiFactory';
 const myPageEvent = {
     userData: async () => {
         var data = await ApiFactory.get("http://localhost:8081/ecommerce/api/user/detail", 4);
-        console.log(data);
         return data;
+    },
+    logout: () => {
+        localStorage.removeItem('accessToken');
+        window.location.href="/login";
     }
 }
 
