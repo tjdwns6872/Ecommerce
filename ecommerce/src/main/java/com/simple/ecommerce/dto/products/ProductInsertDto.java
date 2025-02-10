f27dc2f96e7fed2ac201f47e1da4feb40cc3c53d
tjdwns6872 <tjdwns6872@naver.com>
2025-02-10 13:36:34 +0900

    파라미터 값 NULL 체크 어노테이션 추가 -> 테스트 필요

--- ecommerce/src/main/java/com/simple/ecommerce/dto/products/ProductInsertDto.java
+++ ecommerce/src/main/java/com/simple/ecommerce/dto/products/ProductInsertDto.java
@@ -2,14 +2,17 @@ package com.simple.ecommerce.dto.products;
 
 import java.sql.Date;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor
 public class ProductInsertDto {
     
+    @NotBlank
     private Integer productsId;
 
+    @NotBlank
     private Integer categoryId;
 
     //넘어온 토큰으로 구할 예정
@@ -17,17 +20,24 @@ public class ProductInsertDto {
     
     private String userToken;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String description;
 
+    @NotBlank
     private Integer price;
 
+    @NotBlank
     private Integer stockQuantity;
 
+    @NotBlank
     private Integer status;
 
+    @NotBlank
     private Date createdAt;
 
+    @NotBlank
     private Date updatedAt;
 }
